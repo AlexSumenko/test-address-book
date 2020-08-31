dab5571231ab2a0bc801244632472020e58566aa
Alex Sumenko <alexsumenko@Alexs-MacBook-Pro.local>
2020-08-31 16:35:17 +0300

    finish navbar implementation

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './NavBar.scss';
 
 const NavBar = () => {
-    return (<div className="navbar">Test</div>);
-}
+  return (
+    <nav className='navbar'>
+      <Link to='/' className='navbar__item'>
+        <div>Test</div>
+      </Link>
+      <Link to='/' className='navbar__item'>
+        <div>Test</div>
+      </Link>
+    </nav>
+  );
+};
 
 export default NavBar;
